07c63f7ad7b7c854a0518132090066bb15197610
github username <github email address>
2016-02-16 20:06:08 -0500

    added alot of stuff, mongodb database, node.js, and angular

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -2,25 +2,13 @@ var myApp = angular.module('myApp',[]);
 myApp.controller('AppCtrl',['$scope','$http', function($scope,$http) {
 	console.log("Hello World");
 
-	person1 = {
-		name: 'Tim',
-		emial: 'tim1@gmail.com',
-		number: '(111) 111-1111'
-	};
 
-	person2 = {
-		name: 'emily',
-		emial: 'emily2@gmail.com',
-		number: '(222) 222-2222'
-	};
+	$http.get('/contactList').success(function(response) {
+		console.log("I got the data I requested");
+		$scope.contactList = response
+	})
 
-	person3 = {
-		name: 'John',
-		emial: 'John3@gmail.com',
-		number: '(333) 333-3333'
-	};
 
-	var contactList = [person1, person2, person3];
-	$scope.contactList = contactList;
+	
 }]);
 
